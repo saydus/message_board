9a5b99f4ce0004f90d55c955f278230b561aa1c9
saydus <saydus07@gmail.com>
2020-05-31 14:06:24 -0500

    Added bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) { 
+  res.render('index', { title: 'Messages Board', messages: messages });
 });
+
+router.get('/new', function(req, res, next){
+  res.render('form', {title: 'New message'})
+})
 
 module.exports = router;
