a386f783128c10078870d2065ea0404396296892
saydus <saydus07@gmail.com>
2020-05-31 20:46:30 -0500

    Changed documents

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
-
-
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) { 
-  res.render('index', { title: 'Messages Board', messages: messages });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-
-router.get('/new', function(req, res, next){
-  res.render('form', {title: 'New message'})
-});
-
-
-router.post('/new', function(req, res, next){
-  messages.push({text: req.body.messageText, user: req.body.author, added: new Date()});
-  res.redirect('/');
-})
-
 
 module.exports = router;
